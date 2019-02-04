96c7ef5800010a41c472d53da2dcf51fb42eb35f
Niklas <Niklas@WorkingWithIt>
2019-02-04 08:20:29 +0100

    First Commit

---
+++ moderator/src/main/java/ModeratorBot/moderator/App.java
@@ -0,0 +1,13 @@
+package ModeratorBot.moderator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
