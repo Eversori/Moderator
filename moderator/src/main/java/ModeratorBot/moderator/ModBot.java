daa33b0d6a3b8d7a466729553806c6941c9f24a5
Niklas <Niklas@WorkingWithIt>
2019-02-04 10:10:52 +0100

    builder added

--- moderator/src/main/java/ModeratorBot/moderator/ModBot.java
+++ moderator/src/main/java/ModeratorBot/moderator/ModBot.java
@@ -1,13 +1,16 @@
 package ModeratorBot.moderator;
 
+import net.dv8tion.jda.core.JDABuilder;
+
 /**
  * Hello world!
  *
  */
-public class ModBot 
+public class ModBot
 {
-    public static void main( String[] args )
-    {
-       JDABuilder builder;
-    }
+	public static void main(String[] args)
+	{
+		JDABuilder builder;
+		builder.addEventListener();
+	}
 }
