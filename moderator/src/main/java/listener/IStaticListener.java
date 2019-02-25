19e857eb606b2d83d5a59ab5c610f00410a56ecb
Niklas <Niklas@WorkingWithIt>
2019-02-25 09:36:36 +0100

    NameChangeListener

--- moderator/src/main/java/listener/IStaticListener.java
+++ moderator/src/main/java/listener/IStaticListener.java
@@ -2,10 +2,12 @@ package listener;
 
 public interface IStaticListener
 {
-	public final static String	NICK_CHANGE_USER			= "Der Nutzer ";
-	public final static String	NICK_CHANGE_CALLS_HIMSELF	= " nennt sich nun ";
-	public final static String	NICK_CHANGE_NICK			= "Der Nick ";
-	public final static String	NICK_CHANGE_IN_USE			= " wird schon benutzt";
-	public final static String	NICK_CHANGE_TAKE_OTHER		= "Bitte suche dir einen anderen Namen";
-	public static final String	NAME_CHANGE_SUC				= " changed his name to ";
+	public final static String	NICK_CHANGE_USER			= "The member ";
+	public final static String	NICK_CHANGE_CALLS_HIMSELF	= " calls himself ";
+	public final static String	NICK_CHANGE_NICK			= "The nickname ";
+	public final static String	NICK_CHANGE_IN_USE			= " is already in use";
+	public final static String	NICK_CHANGE_TAKE_OTHER		= "Please select an other nickname";
+	public final static String	NAME_CHANGE_SUC				= " changed his name to ";
+	public final static String	NAME_CHANGE_IN_USE			= " is already in use";
+	public final static String	NAME_CHANGE_TAKE_OTHER		= "Please select an other name";
 }
--- moderator/src/main/java/listener/NameChangeListener.java
+++ moderator/src/main/java/listener/NameChangeListener.java
@@ -2,6 +2,7 @@ package listener;
 
 import ModeratorBot.moderator.ILogMain;
 import factories.MemberFactory;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.user.update.UserUpdateNameEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.managers.GuildController;
@@ -19,10 +20,13 @@ public class NameChangeListener extends ListenerAdapter
 		PrivateMessageWriter writer = new PrivateMessageWriter(event.getEntity());
 		Logger log = LogController.getLogger(ILogMain.NUM, ILogMain.NAME);
 		GuildController controller = event.getJDA().getGuildById(IDiscordId.HOST_GUILD_ID).getController();
+		Member member = null;
 		String msg = "";
 		String newName = event.getNewName();
 		String oldName = event.getOldName();
 		String id = event.getEntity().getId();
+
+		member = MemberFactory.getMemberByID(id).getMember();
 
 		if (MemberFactory.changeName(id, newName))
 		{
@@ -31,7 +35,14 @@ public class NameChangeListener extends ListenerAdapter
 		}
 		else
 		{
+			controller.setNickname(member, oldName);
 
+			msg = newName + IStaticListener.NAME_CHANGE_IN_USE;
+			writer.write(msg);
+			log.addLogMessage(msg, ELogMsgType.ERROR, this.toString(), ILogMain.NAME_CHANGE);
+
+			msg = IStaticListener.NAME_CHANGE_TAKE_OTHER;
+			writer.write(msg);
 		}
 	}
 }
