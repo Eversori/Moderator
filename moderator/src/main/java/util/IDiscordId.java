c92e6de2b73e4d9061557e62e6742461c2cd6734
Niklas <Niklas@WorkingWithIt>
2019-02-25 09:07:38 +0100

    DiscordId - Merge

--- moderator/src/main/java/util/IDiscordId.java
+++ moderator/src/main/java/util/IDiscordId.java
@@ -1,7 +1,11 @@
+
 package util;
 
 public interface IDiscordId
 {
 	public final static String	HOST_GUILD_ID	= "546953492468596759";
 	public final static String	BOT_CHANNEL_ID	= "548081533429219338";
+
+
+  public final static String KICK_CHANNEL_ID = "";
 }
