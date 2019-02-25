15141bd95c5a1df42fd74e79ea9b960b8fd1a065
Niklas <Niklas@WorkingWithIt>
2019-02-25 09:50:46 +0100

    Changes for DiscordWriter

--- moderator/src/main/java/listener/NickNameChangeListener.java
+++ moderator/src/main/java/listener/NickNameChangeListener.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.managers.GuildController;
 import util.DiscordWriter;
 import util.IDiscordId;
+import util.PrivateMessageWriter;
 import util.log.ELogMsgType;
 import util.log.LogController;
 import util.log.Logger;
@@ -19,14 +20,19 @@ public class NickNameChangeListener extends ListenerAdapter
 	public void onGuildMemberNickChange(GuildMemberNickChangeEvent event)
 	{
 		DiscordWriter writer = null;
+		PrivateMessageWriter pwriter = null;
 		Logger log = LogController.getLogger(ILogMain.NUM, ILogMain.NAME);
-		String newName = event.getNewNick();
-		String oldName = event.getPrevNick();
-		String id = event.getMember().getUser().getId();
-		String msg = "";
 		GuildController controller = event.getGuild().getController();
 		TextChannel txt = event.getGuild().getTextChannelById(IDiscordId.BOT_CHANNEL_ID);
 		User user = event.getUser();
+
+		String newName = event.getNewNick();
+		String oldName = event.getPrevNick();
+		String id = user.getId();
+		String msg = "";
+
+		writer = new DiscordWriter(txt);
+		pwriter = new PrivateMessageWriter(user);
 
 		if (oldName == null)
 		{
@@ -38,8 +44,6 @@ public class NickNameChangeListener extends ListenerAdapter
 			newName = user.getName();
 		}
 
-		writer = DiscordWriter.createWriter(writer, txt);
-
 		if (MemberFactory.changeName(id, newName))
 		{
 			msg = IStaticListener.NICK_CHANGE_USER + oldName + IStaticListener.NICK_CHANGE_CALLS_HIMSELF + newName;
@@ -50,11 +54,11 @@ public class NickNameChangeListener extends ListenerAdapter
 		{
 			controller.setNickname(event.getMember(), oldName);
 			msg = IStaticListener.NICK_CHANGE_NICK + newName + IStaticListener.NICK_CHANGE_IN_USE;
-			writer.writePrivate(msg, user);
+			pwriter.write(msg);
 			log.addLogMessage(msg, ELogMsgType.ERROR, this.toString(), ILogMain.NICK_CHANGE);
 
 			msg = IStaticListener.NICK_CHANGE_TAKE_OTHER;
-			writer.writePrivate(msg, user);
+			pwriter.write(msg);
 		}
 
 	}
--- moderator/src/main/java/util/DiscordWriter.java
+++ moderator/src/main/java/util/DiscordWriter.java
@@ -2,120 +2,106 @@
 package util;
 
 import java.awt.Color;
-
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.entities.PrivateChannel;
 import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
-public class DiscordWriter {
+public class DiscordWriter
+{
 
-  private TextChannel channel = null;
-  private GuildMessageReceivedEvent event = null;
-  private EmbedBuilder embedout = null;
-  private static Color warning = Color.RED;
-  private static Color info = Color.ORANGE;
-  private static Color success = Color.GREEN;
+	private TextChannel					channel		= null;
+	private GuildMessageReceivedEvent	event		= null;
+	private EmbedBuilder				embedout	= null;
+	private static Color				warning		= Color.RED;
+	private static Color				info		= Color.ORANGE;
+	private static Color				success		= Color.GREEN;
 
-  public DiscordWriter(TextChannel channel) {
-    this.channel = channel;
-  }
+	public DiscordWriter(TextChannel channel)
+	{
+		this.channel = channel;
+	}
 
-  public static DiscordWriter createWriter(DiscordWriter writer, TextChannel channel) {
-    if (writer == null) {
-      writer = new DiscordWriter(channel);
-    }
-    return writer;
-  }
+	public DiscordWriter(GuildMessageReceivedEvent event)
+	{
+		this.channel = event.getChannel();
+		this.event = event;
+	}
 
-  public static DiscordWriter createWriter(DiscordWriter writer, GuildMessageReceivedEvent event) {
-    if (writer == null) {
-      writer = new DiscordWriter(event.getChannel());
-    }
-    writer.setEvent(event);
-    return writer;
-  }
+	/**
+	 * Writes an Error-Message in the Error-Message-Color. Standard = Red
+	 * 
+	 * @param msg
+	 */
+	public void writeError(String msg)
+	{
+		embedout = new EmbedBuilder().setColor(warning);
+		embedout.setDescription(msg);
+		channel.sendMessage(embedout.build()).queue();
+		embedout = null;
+	}
 
-  /**
-   * Writes an Error-Message in the Error-Message-Color. Standard = Red
-   * 
-   * @param msg
-   */
-  public void writeError(String msg) {
-    embedout = new EmbedBuilder().setColor(warning);
-    embedout.setDescription(msg);
-    channel.sendMessage(embedout.build()).queue();
-    embedout = null;
-  }
+	/**
+	 * Writes a Success-Message in the Success-Message-Color. Standard = Green
+	 * 
+	 * @param msg
+	 */
+	public void writeSuccess(String msg)
+	{
+		embedout = new EmbedBuilder().setColor(success);
+		embedout.setDescription(msg);
+		channel.sendMessage(embedout.build()).queue();
+		embedout = null;
+	}
 
-  /**
-   * Writes a Success-Message in the Success-Message-Color. Standard = Green
-   * 
-   * @param msg
-   */
-  public void writeSuccess(String msg) {
-    embedout = new EmbedBuilder().setColor(success);
-    embedout.setDescription(msg);
-    channel.sendMessage(embedout.build()).queue();
-    embedout = null;
-  }
+	/**
+	 * Writes an Info-Message in the Info-Message-Color. Standard = Orange
+	 * 
+	 * @param msg
+	 */
+	public void writeInfo(String msg)
+	{
+		embedout = new EmbedBuilder().setColor(info);
+		embedout.setDescription(msg);
+		channel.sendMessage(embedout.build()).queue();
+		embedout = null;
+	}
 
-  /**
-   * Writes an Info-Message in the Info-Message-Color. Standard = Orange
-   * 
-   * @param msg
-   */
-  public void writeInfo(String msg) {
-    embedout = new EmbedBuilder().setColor(info);
-    embedout.setDescription(msg);
-    channel.sendMessage(embedout.build()).queue();
-    embedout = null;
-  }
+	/**
+	 * writes a message not embed
+	 * 
+	 * @param msg
+	 */
+	public void writeNotEmbed(String msg)
+	{
+		channel.sendMessage(msg).queue();
+	}
 
-  /**
-   * writes a message not embed
-   * 
-   * @param msg
-   */
-  public void writeNotEmbed(String msg) {
-    channel.sendMessage(msg).queue();
-  }
+	/**
+	 * Writes a custom-colored Message
+	 * 
+	 * @param msg
+	 * @param c
+	 */
+	public void writeCustomColored(String msg, Color c)
+	{
+		embedout = new EmbedBuilder().setColor(c);
+		embedout.setDescription(msg);
+		channel.sendMessage(embedout.build()).queue();
+		embedout = null;
+	}
 
-  /**
-   * Writes a custom-colored Message
-   * 
-   * @param msg
-   * @param c
-   */
-  public void writeCustomColored(String msg, Color c) {
-    embedout = new EmbedBuilder().setColor(c);
-    embedout.setDescription(msg);
-    channel.sendMessage(embedout.build()).queue();
-    embedout = null;
-  }
+	public GuildMessageReceivedEvent getEvent()
+	{
+		return event;
+	}
 
-  /**
-   * Writes a private Message to a User
-   * 
-   * @param msg
-   * @param reciever
-   * User who receives the private Message
-   */
-  public void writePrivate(String msg, User reciever) {
-    PrivateChannel privChan = reciever.openPrivateChannel().complete();
-    privChan.sendMessage(msg).queue();
-  }
+	private void setEvent(GuildMessageReceivedEvent event)
+	{
+		this.event = event;
+	}
 
-  public GuildMessageReceivedEvent getEvent() {
-    return event;
-  }
-
-  private void setEvent(GuildMessageReceivedEvent event) {
-    this.event = event;
-  }
-
-  public TextChannel getChannel() {
-    return channel;
-  }
+	public TextChannel getChannel()
+	{
+		return channel;
+	}
 }
