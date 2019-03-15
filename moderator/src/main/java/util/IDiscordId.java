213b294a35306da556818419c51e77401d7fc442
Niklas <Niklas@WorkingWithIt.fritz.box>
2019-03-15 14:11:51 +0100

    Ids

--- moderator/src/main/java/util/IDiscordId.java
+++ moderator/src/main/java/util/IDiscordId.java
@@ -5,6 +5,9 @@ public interface IDiscordId
 {
 	public final static String	HOST_GUILD_ID	= "546953492468596759";
 	public final static String	BOT_CHANNEL_ID	= "548081533429219338";
-	public final static String	KICK_CHANNEL_ID	= "";
-	public final static String	JOIN_ROLE_ID	= "";
+	public final static String	KICK_CHANNEL_ID	= "556100604297084928";
+	public final static String	JOIN_ROLE_ID	= "556100358049628162";
+	public final static String	ADM_ROLE_ID		= "556100228038524940";
+	public final static String	MOD_ROLE_ID		= "556100289065648128";
+	public final static String	NORM_ROLE_ID	= "556100419504439307";
 }
